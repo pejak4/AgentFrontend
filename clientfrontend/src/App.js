de51c2fcecd619a6374ad086371438aaa00589f6
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-05-12 18:15:21 +0200

    Basic search cars
    From where to where, from when to when

--- clientfrontend/src/App.js
+++ clientfrontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
 import Login from './Login/Login';
 import Registration from './Registration/Registration';
+import SearchPage from './SearchPage/SearchPage';
 
 class App extends React.PureComponent {
   render() {
@@ -13,6 +14,7 @@ class App extends React.PureComponent {
             <Route path="/" exact component={HomePage} />
             <Route path="/login" component={Login} />
             <Route path="/registration" component={Registration} />
+            <Route path="/search" component={SearchPage} />
           </Switch>
         </BrowserRouter>
       </div>
